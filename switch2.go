d900c3aba1e3b5cd23c3b99c06ac6fc712d48f7d
贾浩男 <jiahaonan@bytedance.com>
2019-01-20 15:46:16 +0800

    1.20

---
+++ switch2.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+func main(){
+    t:=time.Now()
+    fmt.Println(t)
+    switch {
+    case t.Hour()<12:
+        fmt.Println("good morning!")
+    case t.Hour()<17:
+        fmt.Println("good afternoon")
+    default:
+        fmt.Println("good evening")
+    }
+}
